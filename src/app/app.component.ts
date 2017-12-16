216a6b43db71c62410bb86c76f9be20e357bfec8
Marcela Peraza <Marce@MacBook-Pro-de-Marcela-2.local>
2017-12-15 18:58:56 -0600

    Tarea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	constructor(private http: HttpClient){
+	}
+
+	ngOnInit(): void {
+		this.http.get('https://api.github.com/users/mperaza22/repos').subscribe(data => {
+			if(Array.isArray(data))
+				this.repos = data;
+		});
+	}
 }
